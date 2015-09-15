600d3cadb7b2e4bf0284f28397ff012de92aaeeb
Tung Mang Vien <tungmv7@gmail.com>
2015-09-15 21:57:12 +0700

    update json

--- sim/modules/admin/helpers/ModuleHelpers.php
+++ sim/modules/admin/helpers/ModuleHelpers.php
@@ -2,13 +2,23 @@
 
 namespace sim\modules\admin\helpers;
 
+use yii\helpers\Json;
 use yii\helpers\VarDumper;
 
 class ModuleHelpers
 {
     public function getModules()
     {
-        $modules = \Yii::getAlias('@sim');
-        VarDumper::dump($modules); exit;
+        $modules = [];
+        $moduleDirs = glob(\Yii::getAlias('@sim/modules') . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR);
+        foreach($moduleDirs as $moduleDir) {
+            if (file_exists($moduleDir . DIRECTORY_SEPARATOR . 'module.json')) {
+                $moduleJson = file_get_contents($moduleDir . DIRECTORY_SEPARATOR . 'module.json');
+                $modules[] = Json::decode($moduleJson);
+            }
+        }
+
+        VarDumper::dump($modules, 10, true);
+        exit;
     }
 }
